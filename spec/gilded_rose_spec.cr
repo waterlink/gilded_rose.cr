ca15ca396b50ec03f8a171f67d9732eaaa7b3649
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 23:21:08 +0100

    [GREEN] Change 118

--- spec/gilded_rose_spec.cr
+++ spec/gilded_rose_spec.cr
@@ -1,10 +1,6 @@
 require "./spec_helper"
 
 describe GildedRose do
-  it "does not fail" do
-    GildedRose.new.update_quality
-  end
-
   it "decreases sell_in and quality of +5 Dexterity Vest" do
     item = Item.new("+5 Dexterity Vest", 10, 20)
     subject = GildedRose.new
