4240e6e1f5cb5ba7c32002408fd257c2b47af3fa
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 14:36:04 +0100

    [GREEN] Change 58

--- spec/gilded_rose_spec.cr
+++ spec/gilded_rose_spec.cr
@@ -172,4 +172,52 @@ describe GildedRose do
     item.sell_in.should eq(-3)
     item.quality.should eq(0)
   end
+
+  it "decreases sell_in and decreases quality x2 of Conjured item" do
+    item = Item.new("Conjured Mana Cake", 10, 20)
+    subject = GildedRose.new
+    subject.items = [item]
+
+    subject.update_quality
+
+    item.name.should eq("Conjured Mana Cake")
+    item.sell_in.should eq(9)
+    item.quality.should eq(19)
+  end
+
+  it "does not change quality when Dexterity Vest has quality <= 0" do
+    item = Item.new("+5 Dexterity Vest", 10, 0)
+    subject = GildedRose.new
+    subject.items = [item]
+
+    subject.update_quality
+
+    item.name.should eq("+5 Dexterity Vest")
+    item.sell_in.should eq(9)
+    item.quality.should eq(0)
+  end
+
+  it "reduces quality at a 2x speed when Dexterity Vest has sell_in <= 0" do
+    item = Item.new("+5 Dexterity Vest", 0, 20)
+    subject = GildedRose.new
+    subject.items = [item]
+
+    subject.update_quality
+
+    item.name.should eq("+5 Dexterity Vest")
+    item.sell_in.should eq(-1)
+    item.quality.should eq(18)
+  end
+
+  it "reduces only sell_in when Dexterity Vest has sell_in <= 0 and quality <= 0" do
+    item = Item.new("+5 Dexterity Vest", 0, 0)
+    subject = GildedRose.new
+    subject.items = [item]
+
+    subject.update_quality
+
+    item.name.should eq("+5 Dexterity Vest")
+    item.sell_in.should eq(-1)
+    item.quality.should eq(0)
+  end
 end
