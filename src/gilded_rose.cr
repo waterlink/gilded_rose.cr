51b4d898f9675aeef32daf893b4e2b7201e138ef
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 14:42:36 +0100

    [GREEN] Change 81

--- src/gilded_rose.cr
+++ src/gilded_rose.cr
@@ -29,7 +29,7 @@ class GildedRose
           @items[i].quality = @items[i].quality + 1 # OK
           if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert") # OK
             if (@items[i].sell_in < 11) # OK
-              if (@items[i].quality < 50)
+              if (@items[i].quality < 50) # OK
                 @items[i].quality = @items[i].quality + 1
               end
             end
