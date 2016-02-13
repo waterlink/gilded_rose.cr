b9584bf922c1f3d6cfd7d9403f5645ceb94bce4e
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 13:58:28 +0100

    [GREEN] Change 4

--- spec/gilded_rose_spec.cr
+++ spec/gilded_rose_spec.cr
@@ -5,7 +5,7 @@ describe GildedRose do
     GildedRose.new.update_quality
   end
 
-  it "does something to +5 Dexterity Vest" do
+  it "decreases sell_in and quality of +5 Dexterity Vest" do
     item = Item.new("+5 Dexterity Vest", 10, 20)
     subject = GildedRose.new
     subject.items = [item]
