7053f54d7cf84c9e8b26e874e2eb51ed5c0463a1
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 23:46:17 +0100

    [GREEN] Change 142

--- src/item.cr
+++ src/item.cr
@@ -10,25 +10,42 @@ class Item
 end
 
 class Normal
+  private getter item
   def initialize(@item)
   end
 
   def update
-    @item.sell_in -= 1
-    @item.quality -= 1
-    @item.quality -= 1 if @item.sell_in < 0
-    @item.quality = 0 if @item.quality < 0
+    item.sell_in -= 1
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 0
+    item.quality = 0 if item.quality < 0
   end
 end
 
 class Brie
+  private getter item
   def initialize(@item)
   end
 
   def update
-    @item.sell_in -= 1
-    @item.quality += 1
-    @item.quality += 1 if @item.sell_in < 0
-    @item.quality = 50 if @item.quality > 50
+    item.sell_in -= 1
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 0
+    item.quality = 50 if item.quality > 50
+  end
+end
+
+class BackstagePass
+  private getter item
+  def initialize(@item)
+  end
+
+  def update
+    item.sell_in -= 1
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 11
+    item.quality += 1 if item.sell_in < 6
+    item.quality = 50 if item.quality > 50
+    item.quality = 0 if item.sell_in < 0
   end
 end
