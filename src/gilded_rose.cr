4214be5836594232960c9df95968e7fb91cc1059
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 14:26:02 +0100

    [GREEN] Change 45

--- src/gilded_rose.cr
+++ src/gilded_rose.cr
@@ -18,14 +18,14 @@ class GildedRose
 
     (0..(@items.size-1)).each do |i|
       if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@items[i].quality > 0) # covered
+        if (@items[i].quality > 0)
           if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1 # covered
+            @items[i].quality = @items[i].quality - 1
           end
         end
-      else # covered
-        if (@items[i].quality < 50) # covered
-          @items[i].quality = @items[i].quality + 1  # covered
+      else
+        if (@items[i].quality < 50)
+          @items[i].quality = @items[i].quality + 1
           if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
             if (@items[i].sell_in < 11)
               if (@items[i].quality < 50)
@@ -42,15 +42,15 @@ class GildedRose
       end
 
       if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1 # covered
+        @items[i].sell_in = @items[i].sell_in - 1
       end
 
-      if (@items[i].sell_in < 0)  # covered
+      if (@items[i].sell_in < 0)
         if (@items[i].name != "Aged Brie")
           if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
             if (@items[i].quality > 0)
               if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1 # covered
+                @items[i].quality = @items[i].quality - 1
               end
             end
           else
