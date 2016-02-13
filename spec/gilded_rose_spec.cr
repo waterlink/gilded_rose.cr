013a6862934a6a9a2915ba9cb06a44ae6b63bc74
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 14:11:15 +0100

    [GREEN] Change 19

--- spec/gilded_rose_spec.cr
+++ spec/gilded_rose_spec.cr
@@ -16,4 +16,16 @@ describe GildedRose do
     item.sell_in.should eq(9)
     item.quality.should eq(19)
   end
+
+  it "... when Dexterity Vest has quality == 0" do
+    item = Item.new("+5 Dexterity Vest", 10, 0)
+    subject = GildedRose.new
+    subject.items = [item]
+
+    subject.update_quality
+
+    item.name.should eq("+5 Dexterity Vest")
+    item.sell_in.should eq(9)
+    item.quality.should eq(0)
+  end
 end
