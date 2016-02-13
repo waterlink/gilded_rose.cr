d4038d615cd3f6edb2bed9a9086a5420a09d595d
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 23:58:50 +0100

    [GREEN] Change 160

--- spec/gilded_rose_spec.cr
+++ spec/gilded_rose_spec.cr
@@ -3,10 +3,9 @@ require "./spec_helper"
 describe GildedRose do
   it "decreases sell_in and quality of +5 Dexterity Vest" do
     item = Item.new("+5 Dexterity Vest", 10, 20)
-    subject = GildedRose.new
-    subject.items = [item]
+    subject = ItemUpdater.new(item)
 
-    subject.update_quality
+    subject.update
 
     item.name.should eq("+5 Dexterity Vest")
     item.sell_in.should eq(9)
@@ -15,10 +14,9 @@ describe GildedRose do
 
   it "does not change quality when Dexterity Vest has quality <= 0" do
     item = Item.new("+5 Dexterity Vest", 10, 0)
-    subject = GildedRose.new
-    subject.items = [item]
+    subject = ItemUpdater.new(item)
 
-    subject.update_quality
+    subject.update
 
     item.name.should eq("+5 Dexterity Vest")
     item.sell_in.should eq(9)
