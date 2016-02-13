ae6680bcbf215ecdbe53897e0f4afcb572bbe00e
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-14 00:02:18 +0100

    [GREEN] Change 164

--- src/gilded_rose.cr
+++ src/gilded_rose.cr
@@ -10,12 +10,12 @@ class GildedRose
     end
 
     @items = [] of Item
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+    items << Item.new("+5 Dexterity Vest", 10, 20)
+    items << Item.new("Aged Brie", 2, 0)
+    items << Item.new("Elixir of the Mongoose", 5, 7)
+    items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    items << Item.new("Conjured Mana Cake", 3, 6)
   end
 
   def ==(other : GildedRose)
